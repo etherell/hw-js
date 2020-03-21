767e30c4aebedfe5bdb58ea0d14934a215dcd138
etherell <60687843+etherell@users.noreply.github.com>
2020-03-21 09:14:26 +0200

    Update hw-5.js

--- 5/hw-5.js
+++ 5/hw-5.js
@@ -66,69 +66,12 @@ formButton.onclick = function(){
 }; */
 
 
-
 button.addEventListener('click', function(event){
         result_wrapper.style.display = "block";
         let translitText = input.value;
         result.innerHTML = `Результат транслитерации: <br><b>${translate(translitText)}</b>`;
         input.value = "";
 });
-
-
-
- console.log('***Рекурсия***')
- console.log('***Числа от А до Б***')
-
- function from_a_to_b(a, b){
-        console.log(a)
-        if (a < b){
-                a++
-                from_a_to_b(a, b)
-        } else if (a > b){
-                a--
-                from_a_to_b(a, b)
-        } else {
-                return a
-        };      
-  };
-
-from_a_to_b(5, 10)
-
- console.log('***Сумма цифер числа***')
-
- let sum = 0;
- let i = 1
-
- function num_sum(number){
-        sum += Math.floor(number / i % 10)
-
-        if(i <= number){
-                i *= 10;
-                num_sum(number);
-        };
-
-        return sum;     
- };
-
- console.log(num_sum(149));
-
-
-console.log('***Перебор меню***')
-
-let menu = ["about us", "contacts", "main", "products"];
-console.log(menu);
-let newMenu = [];
-
-menu.forEach(function(item, index){
-        item = item.split("");
-        item.splice(0, 1, item[0].toUpperCase());``  //item[0] = item[0].toUpperCase();
-        item = item.join("");
-        newMenu.push(item);
-        console.log(item);
-});
-
-console.log(newMenu);
-
 
 // Работа над картинкой
 let cat    = document.querySelector(".cat");
